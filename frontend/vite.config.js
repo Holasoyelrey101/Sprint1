29098add92e9f31b90ee632e607fb6afdd831358
Holasoyelrey101 <sir.uk101@gmail.com>
2026-03-31 23:32:12 -0300

    Add preview configuration to Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['frontend-production-98d34.up.railway.app'],
+  },
   server: {
     proxy: {
       '/api': {
